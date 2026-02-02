ed17306b00583c260cfbb41fa20155a2d1ec1808
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-02-02 10:43:02 +0000

    Another refactor.

---
+++ src/main/java/net/gourmand/core/registry/blocks/CoreDefaultCropBlock.java
@@ -0,0 +1,32 @@
+package net.gourmand.core.registry.blocks;
+
+import net.dries007.tfc.common.blocks.ExtendedProperties;
+import net.dries007.tfc.common.blocks.TFCBlockStateProperties;
+import net.dries007.tfc.common.blocks.crop.DefaultCropBlock;
+import net.dries007.tfc.util.climate.ClimateRange;
+import net.gourmand.core.registry.CoreBlocks;
+import net.gourmand.core.registry.CoreItems;
+import net.gourmand.core.registry.category.CoreClimateRanges;
+import net.gourmand.core.registry.category.CoreCrops;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
+
+public abstract class CoreDefaultCropBlock extends DefaultCropBlock {
+
+    public static CoreDefaultCropBlock create(ExtendedProperties properties, int stages, CoreCrops crop) {
+        final IntegerProperty property = TFCBlockStateProperties.getAgeProperty(stages - 1);
+        return new CoreDefaultCropBlock(properties, stages - 1, CoreBlocks.DEAD_CROPS.get(crop), CoreItems.CROP_SEEDS.get(crop), crop.getNitrogen(), crop.getPhosphorous(), crop.getPotassium(), CoreClimateRanges.CROPS.get(crop)) {
+            public @NotNull IntegerProperty getAgeProperty() {
+                return property;
+            }
+        };
+    }
+
+    protected CoreDefaultCropBlock(ExtendedProperties properties, int maxAge, Supplier<? extends Block> dead, Supplier<? extends Item> seeds, float nitrogen, float phosphorous, float potassium, Supplier<ClimateRange> climateRange) {
+        super(properties, maxAge, dead, seeds, nitrogen, phosphorous, potassium, climateRange);
+    }
+}
---
+++ src/main/java/net/gourmand/core/util/RegistryClay.java
@@ -0,0 +1,14 @@
+package net.gourmand.core.util;
+
+import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.level.material.MapColor;
+
+public interface RegistryClay extends StringRepresentable {
+
+    public MapColor mapColor();
+
+    public Rarity getRarity();
+
+    public boolean hasReducedSet();
+}
